746343af215713078c8e01806bc7e8f2670c93ca
Birdbird <me@birdbird.net>
2013-01-21 12:08:23 +0800

    second commit

--- control.py
+++ control.py
@@ -4,11 +4,20 @@ from flask import render_template
 import feedparser
 from future import Future
 from public.publicFunction import *
+from sites.douban import *
 app = Flask(__name__)
 
 @app.route('/')
 def Index():
 	return render_template('index.html',content=ReadMultiRss())
+
+@app.route('/doubanauth')
+def DoubanAuthor():
+	return render_template('douban.html',content=DoubanAuth())
+
+@app.route('/auth')
+def AfterAuth():
+	return render_template('auth.html',content=DoumaoAuth())
 
 if __name__ == '__main__':
     app.run(debug=True)
--- sites/douban.py
+++ sites/douban.py
@@ -1,12 +1,32 @@
 #-*- coding: UTF-8 -*-
 from flask import Flask
-from flask import render_template
+from flask import render_template,redirect, url_for
 import feedparser
 from future import Future
 from public.publicFunction import *
+from douban_client import DoubanClient
 app = Flask(__name__)
 
 def BookRss(uid):
 	url = 'http://api.douban.com/people/'+uid
 	dom3 = parseString(result)
 	root = dom3.documentElement
+
+def DoubanAuth():
+	API_KEY = '0e599b6718a074832b0cd23549e15d89'
+	API_SECRET = '0ce584ebcdda1f87'
+	redirect_uri = 'http://doumao.net/auth'
+
+	SCOPE = 'douban_basic_common,shuo_basic_r,shuo_basic_w'
+
+	client = DoubanClient(API_KEY, API_SECRET, redirect_uri, SCOPE)
+
+	result = client.authorize_url
+	# result = client.auth_with_code("birdbirdcatcat")
+
+	# return redirect(url_for(result))
+	return result
+
+def DoumaoAuth():
+	result = "Welecome to Doumao"
+	return result
